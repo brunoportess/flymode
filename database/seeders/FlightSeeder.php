cc3f2e617307c9614cdb075540cdc5d8542a5ce4
bruno <bruno.portess@gmail.com>
2025-02-05 23:43:25 -0300

    add order seed

--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\FlightOrder;
+use App\Models\Location;
+use Carbon\Carbon;
+use Carbon\Exceptions\Exception;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +16,22 @@ class FlightSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        FlightOrder::factory()->create([
+            'requesting_user_id' => 1,
+            'destination_id' => 1,
+            'departure_date' => Carbon::now()->addDays(5)->format("Y-m-d"),
+            'return_date' => Carbon::now()->addDays(10)->format("Y-m-d"),
+            'status' => 'S',
+            'status_text' => 'Solicitado',
+        ]);
+
+        FlightOrder::factory()->create([
+            'requesting_user_id' => 1,
+            'destination_id' => 1,
+            'departure_date' => Carbon::now()->addDays(12)->format("Y-m-d"),
+            'return_date' => Carbon::now()->addDays(17)->format("Y-m-d"),
+            'status' => 'S',
+            'status_text' => 'Solicitado',
+        ]);
     }
 }
