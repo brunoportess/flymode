8fb418f4e1e2800852af07c2d04042eab1d13905
bruno <bruno.portess@gmail.com>
2025-02-05 23:43:37 -0300

    Create default controllers

---
+++ app/Http/Controllers/FlightOrderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FlightOrderController extends Controller
+{
+    function index()
+    {
+
+    }
+
+    function find($id)
+    {
+
+    }
+
+    function store(Request $request)
+    {
+
+    }
+
+    function update($id, Request $request)
+    {
+
+    }
+
+    function destroy($id)
+    {
+
+    }
+}
