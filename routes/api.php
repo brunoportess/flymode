16e4af712b08e438b86fde5aa06534db13883387
bruno <bruno.portess@gmail.com>
2025-02-08 23:13:03 -0300

    set .env data

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('orders')->group(function () {
         Route::get('/busca', \App\Http\Controllers\SearchFlightOrderController::class)->name('orders.search');
         Route::get('/status/{status}', [\App\Http\Controllers\FlightOrderController::class, 'getByStatus'])->name('orders.status');
+        Route::put('/status/{id}', \App\Http\Controllers\StatusFlightOrderController::class)->name('orders.status.update');
         Route::get('/{id}', [\App\Http\Controllers\FlightOrderController::class, 'find'])->name('orders.find');
         Route::post('/', [\App\Http\Controllers\FlightOrderController::class, 'store'])->name('orders.store');
-        Route::put('/status/{id}', \App\Http\Controllers\StatusFlightOrderController::class)->name('orders.status.update');
+
     });
 });
